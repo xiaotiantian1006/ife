8f9106a127a8d8de2ad99c78c15cd643e130171a
xiaotiantian1006 <532078170@qq.com>
2017-03-15 13:54:25 +0800

    左进左出右进右出

---
+++ queue/js/index.js
@@ -0,0 +1,66 @@
+	 var aDiv=document.getElementsByTagName('p');
+	//判断input输入是否为数字
+		function inputText(){
+			var input=document.getElementsByTagName('input')[0].value;
+			if(isNaN(input)){
+				alert('请输入数字');
+			}else{
+				return input;
+			}
+		}
+//创建p
+		function arrDiv(input){
+			var eDiv=document.createElement('p');
+			console.log(eDiv);
+			eDiv.appendChild(document.createTextNode(input));
+			return eDiv;
+		}
+//移除div
+		function reDiv(){
+			document.body.removeChild(this);
+		}
+		//点击四个按钮
+		
+		function buttonClick(){
+			//左侧入
+			document.getElementsByClassName('leftIn')[0].onclick=function(){
+				var input=inputText();
+				if(input){
+				 var divEle=arrDiv(input);
+					if(aDiv.length){
+						document.body.insertBefore(divEle,aDiv[0]);
+					}else{
+						document.body.appendChild(divEle);
+					}
+					divEle.onclick=reDiv;
+				}
+				document.getElementsByTagName("input")[0].value = "";
+			};
+			//右侧入
+			document.getElementsByClassName('rightIn')[0].onclick=function(){
+				var input=inputText();
+				if(input){					
+					var divEle=arrDiv(input);
+					document.body.appendChild(divEle);
+					divEle.onclick=reDiv;
+				}
+				document.getElementsByTagName("input")[0].value = "";
+			};
+			//左侧出
+			document.getElementsByClassName('leftOut')[0].onclick=function(){
+				if(aDiv.length){
+					var value = aDiv[0].innerHTML;
+					document.body.removeChild(aDiv[0]);
+					alert(value);
+				}
+			}
+			//右侧出
+				document.getElementsByClassName("rightOut")[0].onclick = function(){
+					if(aDiv.length){
+						var value = aDiv[aDiv.length - 1].innerHTML;
+						var reomveElem = document.body.removeChild(aDiv[aDiv.length - 1]);
+						alert(value);
+					}
+				}
+		}
+		buttonClick()
